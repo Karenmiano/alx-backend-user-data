6c4c2f35ef84a18a40434094c59b4120a0b90f52
Karenmiano <mianokaren1798@gmail.com>
2024-05-15 13:34:10 +0300

    redact values of data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,15 +3,16 @@
 Defines the function filter_datum.
 """
 import re
+from typing import List
 
 
-def filter_datum(
-        fields: str, redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Replaces the values of specified fields in a message
     with a redaction string.
     """
     for field in fields:
-        message = re.sub(field + r'=[^{}]*'.format(separator),
-                         field + "=" + redaction, message)
+        message = re.sub(r'{}=[^{}]*'.format(field, separator),
+                         "{}={}".format(field, redaction), message)
     return message
