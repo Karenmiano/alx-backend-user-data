add7ecbe149ea7fe7f0a087b2017cfe223764d5c
Karenmiano <mianokaren1798@gmail.com>
2024-05-16 08:43:02 +0300

    create Logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,10 +28,14 @@ def get_logger() -> logging.Logger:
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
+    logger.propagate = False
+
     formatter = RedactingFormatter(list(PII_FIELDS))
+
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
+    
     return logger
 
 
