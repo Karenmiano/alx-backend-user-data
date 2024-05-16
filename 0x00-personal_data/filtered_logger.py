b2a848b0be0e2fca7cc669d3c027fd9ed7013edf
Karenmiano <mianokaren1798@gmail.com>
2024-05-16 11:20:03 +0300

    Log Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,8 +10,7 @@ from typing import List
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Replaces the values of fields with a redaction string."""
     for field in fields:
         message = re.sub(r'{}=[^{}]*'.format(field, separator),
