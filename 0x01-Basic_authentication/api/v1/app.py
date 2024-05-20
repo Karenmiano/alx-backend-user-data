b208170d55b4a30a8165f67b5d6fd2d9de968304
Karenmiano <mianokaren1798@gmail.com>
2024-05-20 18:53:34 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,11 @@ if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+excluded_paths = [
+    '/api/v1/status/',
+    '/api/v1/unauthorized/',
+    '/api/v1/forbidden/'
+    ]
 
 
 @app.before_request
@@ -26,12 +30,11 @@ def before_request():
     if auth is None:
         return
     elif auth.require_auth(request.path, excluded_paths):
-        return
+        pass
     elif auth.authorization_header(request) is None:
         abort(401)
     elif auth.current_user(request) is None:
         abort(403)
-
 
 
 @app.errorhandler(404)
