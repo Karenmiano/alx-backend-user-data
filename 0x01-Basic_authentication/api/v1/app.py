adbbff5831d67ce99b044718f5a31b8aea28957b
Karenmiano <mianokaren1798@gmail.com>
2024-05-20 18:38:30 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,26 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+
+@app.before_request
+def before_request():
+    if auth is None:
+        return
+    elif auth.require_auth(request.path, excluded_paths):
+        return
+    elif auth.authorization_header(request) is None:
+        abort(401)
+    elif auth.current_user(request) is None:
+        abort(403)
+
 
 
 @app.errorhandler(404)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""
+Authentication module.
+"""
 from flask import request
 from typing import List, TypeVar
 
 class Auth:
+    """
+    Defines the auth class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if path requires authentication.
@@ -16,7 +22,15 @@ class Auth:
         return False
     
     def authorization_header(self, request=None) -> str:
-        return None
+        """
+        Retrieves value passed in Authorization header in request.
+        """
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
     
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user.
+        """
         return None
