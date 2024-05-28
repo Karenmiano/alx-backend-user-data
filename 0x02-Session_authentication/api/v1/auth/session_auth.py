6153e0c1eafffdfb307f27886660ab61aa2869b1
Karenmiano <mianokaren1798@gmail.com>
2024-05-28 13:44:32 +0300

    User id from session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -31,6 +31,6 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
 
-        user_id = SessionAuth.user_id_by_session_id.get("session_id")
+        user_id = SessionAuth.user_id_by_session_id.get(session_id)
 
         return user_id
