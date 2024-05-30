1b2cb029a8c4dd9511842a762698c4f7d6c8ebf5
Karenmiano <mianokaren1798@gmail.com>
2024-05-30 13:58:31 +0300

    create User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,7 +35,7 @@ class DB:
         """
         Creates and adds a new user to the database.
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
+        return user
