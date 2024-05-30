e6fd10106db45770fcacd938025ea530a82aab91
Karenmiano <mianokaren1798@gmail.com>
2024-05-31 02:10:32 +0300

    update user

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for authentication.
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Return hashed password
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password
